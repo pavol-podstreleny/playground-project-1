7e0c5ff8035d123d060f07c6e54daec628e2242f
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-05-28 14:31:58 +0800

    Implemented basic HTTP GET endpoint for retrieving customers

---
+++ back-end/CustomerAPI/CustomerAPI/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using CustomerAPI.Model;
+using CustomerAPI.services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Controllers
+{
+
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService CustomerService)
+        {
+            this._customerService = CustomerService;
+        }
+
+
+        [HttpGet]
+        public ActionResult<Customer> GetAllCustomers()
+        {
+            IEnumerable<Customer> customers = _customerService.GetAllCustomers();
+            return Ok(customers);
+        }
+
+    }
+}
