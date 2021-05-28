1f009fd776a3447e396a80c4c22ca23d30680804
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-05-28 14:30:48 +0800

    Created ICustomerService that provides method declaration

---
+++ back-end/CustomerAPI/CustomerAPI/Services/CustomerService.cs
@@ -0,0 +1,49 @@
+﻿using CustomerAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.services
+{
+    public class CustomerService : ICustomerService
+    {
+
+        private readonly ICustomerRepository customerRepository;
+
+        public CustomerService(ICustomerRepository CustomerRepository)
+        {
+            this._customerRepository = CustomerRepository;
+        }
+
+        public int CreateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteCustomer(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetCustomersAfter(string id, int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer UpdateCustomer(string id, Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ back-end/CustomerAPI/CustomerAPI/Services/ICustomerService.cs
@@ -0,0 +1,18 @@
+﻿using CustomerAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.services
+{
+    public interface ICustomerService
+    {
+        public IEnumerable<Customer> GetAllCustomers();
+        public IEnumerable<Customer> GetCustomersAfter(string id, int limit);
+        public Customer GetCustomer(string id);
+        public Customer UpdateCustomer(string id, Customer customer);
+        public int DeleteCustomer(string id);
+        public int CreateCustomer(Customer customer);
+    }
+}
