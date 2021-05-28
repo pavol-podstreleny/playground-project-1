bcd68b74b25f22cc3861b03225b31ec05d45355b
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-05-28 14:32:44 +0800

    Added CustomerService into DI container

--- back-end/CustomerAPI/CustomerAPI/Startup.cs
+++ back-end/CustomerAPI/CustomerAPI/Startup.cs
@@ -1,3 +1,4 @@
+using CustomerAPI.services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,6 +28,9 @@ namespace CustomerAPI
         {
 
             services.AddControllers();
+
+            // Add custom services
+            services.AddScoped<ICustomerService, CustomerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
