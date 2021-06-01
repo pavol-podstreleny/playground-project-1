ed97f65ba233000a365ca313990f6ca07c2ea919
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-06-01 12:43:13 +0800

    Remove type in appsetings and create config file for StorageTableConnection

---
+++ back-end/CustomerAPI/CustomerAPI/Utils/StorageTableConnectionConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Utils
+{
+    public class StorageTableConnectionConfig
+    {
+        public string DefaultEndpointsProtocol { get; set; }
+        public string AccountKey { get; set; }
+        public string TableEndpoint { get; set; }
+
+        public string GetConnectionString()
+        {
+            Console.WriteLine($"{DefaultEndpointsProtocol};{AccountKey};{TableEndpoint};");
+            return $"{DefaultEndpointsProtocol};{AccountKey};{TableEndpoint};";
+        }
+
+    }
+}
