b71964cb3d2118d009925459aa7be871ed54e515
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-06-15 18:24:11 +0800

    Removed output dependency

--- back-end/CustomerAPI/CustomerAPI.Test/Utils/StorageTableConnectionConfigTest.cs
+++ back-end/CustomerAPI/CustomerAPI.Test/Utils/StorageTableConnectionConfigTest.cs
@@ -6,12 +6,6 @@ namespace CustomerAPI.Test.Utils {
     
     public class StorageTableConnectionConfigTest {
 
-        private readonly ITestOutputHelper _output;
-
-        public StorageTableConnectionConfigTest(ITestOutputHelper output){
-            this._output = output;
-        }
-
         [Theory]
         [InlineData("AccountKey=1234","DefaultEndpointsProtocol=1525","TableEndpoint=http://localhost","DefaultEndpointsProtocol=1525;AccountKey=1234;TableEndpoint=http://localhost;")]
         [InlineData("AccountKey=asdfadf","DefaultEndpointsProtocol=adsfasdf123","","DefaultEndpointsProtocol=adsfasdf123;AccountKey=asdfadf;;")]
@@ -26,12 +20,9 @@ namespace CustomerAPI.Test.Utils {
                 DefaultEndpointsProtocol=defaultProtocol,
                 TableEndpoint=tableEndpoint
             };
-            _output.WriteLine(expected);
 
             Assert.Equal(expected,storageConfig.GetConnectionString());
         }
-
-    
 
     }
 }
