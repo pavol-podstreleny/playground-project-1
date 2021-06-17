b4a8b42830278d2483b2300fc4c1fda4699d5f2c
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-06-17 23:18:09 +0800

    Added primary button component

---
+++ front-end/src/components/primarybutton.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const PrimaryButton = styled.button`
+  display: block;
+  padding: 10px 14px;
+  color: white;
+  background: rgba(0, 120, 212, 1);
+  border: none;
+  border-radius: 2px;
+  cursor: pointer;
+  text-align: center;
+
+  &:hover {
+    background: #066cbb;
+  }
+`;
