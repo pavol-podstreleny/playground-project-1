ec3e5b19261e206dd26e793ccf3e94ab620de5df
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-06-18 18:03:24 +0800

    Provide options to publish.sh script

--- back-end/ci/publish.sh
+++ back-end/ci/publish.sh
@@ -1,2 +1,2 @@
-docker login -u $DOCKERLOGIN -p $DOCKERCREDENTIALS
+docker login --username $DOCKERLOGIN --password $DOCKERCREDENTIALS
 docker-compose -f ../docker-compose-backend.yaml -f ../docker-compose-backend-build.yaml push
