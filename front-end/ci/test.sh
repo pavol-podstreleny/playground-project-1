a0c778f014adff4ad5bf5caf7990bb037a556313
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-06-19 00:08:13 +0800

    Updated test.sh script run test app in detached mode

--- front-end/ci/test.sh
+++ front-end/ci/test.sh
@@ -1,4 +1,4 @@
 # Smoke test
-docker-compose -f ../docker-compose-frontend.yaml -f ../docker-compose-frontend-dev.yaml up
+docker-compose -f ../docker-compose-frontend.yaml -f ../docker-compose-frontend-dev.yaml up -d
 docker-compose ps
 docker-compose -f ../docker-compose-frontend.yaml -f ../docker-compose-frontend-dev.yaml down
