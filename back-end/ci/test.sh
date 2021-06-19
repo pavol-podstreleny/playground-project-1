9f300bdf4a94b12b78676ac513f401edba2d1ccf
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-06-19 17:56:30 +0800

    Fixed problem with secrets and test script

--- back-end/ci/test.sh
+++ back-end/ci/test.sh
@@ -1,9 +1,10 @@
 # Test Customer API
 docker-compose -f ../docker-compose-backend-test.yaml up
 # Copy results of the test to agent directory
-docker cp back-end_customer-api-test:/app/test/CustomerAPI.Test/TestResults $SYSTEM_DEFAULTWORKINGDIRECTORY
+
+docker cp back-end_customer-api-test_1:/app/test/CustomerAPI.Test/TestResults $SYSTEM_DEFAULTWORKINGDIRECTORY
 
 # Smoke test
-docker-compose -f ../docker-compose-backend.yaml -f ../docker-compose-backend-dev.yaml up
+docker-compose -f ../docker-compose-backend.yaml -f ../docker-compose-backend-dev.yaml up -d
 docker-compose ps
 docker-compose -f ../docker-compose-backend.yaml -f ../docker-compose-backend-dev.yaml down
