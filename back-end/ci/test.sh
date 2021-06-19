9907bc40ac8d7c1d47a4d595f99c35c9abfa4a72
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-06-19 17:12:51 +0800

    Use publish test result task in pipeline

--- back-end/ci/test.sh
+++ back-end/ci/test.sh
@@ -1,5 +1,7 @@
 # Test Customer API
 docker-compose -f ../docker-compose-backend-test.yaml up
+# Copy results of the test to agent directory
+docker cp back-end_customer-api-test:/app/test/CustomerAPI.Test/TestResults $SYSTEM_DEFAULTWORKINGDIRECTORY
 
 # Smoke test
 docker-compose -f ../docker-compose-backend.yaml -f ../docker-compose-backend-dev.yaml up
