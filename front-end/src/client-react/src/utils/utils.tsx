4516e5f1c805b47950b08f5893e432afdbee69f3
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-07-08 15:33:08 +0800

    Added validate method for forms

--- front-end/src/client-react/src/utils/utils.tsx
+++ front-end/src/client-react/src/utils/utils.tsx
@@ -4,6 +4,25 @@ export interface ValidKey {
   [state: string]: string | number | undefined;
 }
 
+export function validate<T extends ValidKey, K extends keyof T>(
+  schema: Joi.ObjectSchema<any>,
+  data: any,
+  errorObject: T
+): T | null {
+  const { error } = schema.validate(data, { abortEarly: false });
+  let containsError = false;
+  if (error) {
+    for (let detail of error.details) {
+      errorObject[detail.path as unknown as K] = detail.message as T[K];
+      containsError = true;
+    }
+  }
+  if (!containsError) {
+    return null;
+  }
+  return errorObject;
+}
+
 export function validateProperty<T extends ValidKey, K extends keyof T>(
   schema: Joi.ObjectSchema<any>,
   key: K,
