e3f1a3da9e5eaa54efd1aef6b4c6c29973739368
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-07-12 14:53:35 +0800

    Updated css and imported normalize css in index.tsx

--- front-end/src/client-react/src/index.tsx
+++ front-end/src/client-react/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import "./css/normalize.css";
 import "./components/common/buttons/buttons.css";
 import "./css/base.css";
 
