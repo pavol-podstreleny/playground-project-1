0caf804139f8bbe47a961407d3464fee8b6b649e
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-07-27 16:52:45 +0800

    Moved customer page and table to separate directory

---
+++ front-end/src/client-react/src/components/customerPage/customerPage.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import Card from "../common/cards/card";
+import PageSizeSelector from "../common/pagination/pageSizeSelector";
+import Pagination from "../common/pagination/pagination";
+import CustomerDialogAdd from "../customerDialogAdd";
+import CustomerDialogDelete from "../customerDialogDelete";
+import CustomerDialogEdit from "../customerDialogEdit";
+import CustomerTable from "../customerTable/customerTable";
+import TryAgain from "../common/errors/tryAgain";
+import { useFetchCustomers } from "../../hooks/customers/useFetchCustomers";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import {
+  customerAddDialogShowed,
+  customerCurrentPaginationPageChanged,
+  customerPaginationPageSizeChanged,
+} from "../../store/customers";
+import Loader from "../common/loading/loader";
+import { useAppSelector } from "../../hooks/useAppSelector";
+import "./customerPage.css";
+import { WidthContext } from "../context/widthContext";
+import { useRef } from "react";
+
+const pageOptions = [1, 2, 5, 10, 25, 50, 100];
+
+export const CustomerPage = () => {
+  const [customers, isLoading, errorMessages, refetch] = useFetchCustomers();
+  const dispatch = useAppDispatch();
+  const currentPage = useAppSelector(
+    (state) => state.entities.customers.pagination.currentPage
+  );
+  const pageSize = useAppSelector(
+    (state) => state.entities.customers.pagination.pageSize
+  );
+
+  const handlePageChange = (pageNumber: number) => {
+    dispatch(customerCurrentPaginationPageChanged(pageNumber));
+  };
+
+  const handlePageSizeChange = (pageSize: number) => {
+    dispatch(customerCurrentPaginationPageChanged(1));
+    dispatch(customerPaginationPageSizeChanged(pageSize));
+  };
+
+  const handleAddCustomerButtonClick = () => {
+    dispatch(customerAddDialogShowed());
+  };
+
+  if (errorMessages) {
+    return (
+      <div className="force-center">
+        <TryAgain onClick={refetch} />
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return <Loader text="Loading..." />;
+  }
+
+  if (!customers) {
+    return null;
+  }
+  return (
+    <React.Fragment>
+      <section className="customer-page">
+        <div className="center">
+          <Card>
+            <div className="customer-header">
+              <h1 className="customer-header__heading">Customers Table</h1>
+              <div className="customer-header__action-group">
+                <PageSizeSelector
+                  actualPageSize={pageSize}
+                  onPageSizeChange={handlePageSizeChange}
+                  pageOptions={pageOptions}
+                />
+                <button
+                  className="button button--success"
+                  onClick={handleAddCustomerButtonClick}
+                >
+                  Add Customer
+                </button>
+              </div>
+            </div>
+            <CustomerTable
+              customers={customers}
+              pagination={{
+                pageNumber: currentPage,
+                pageSize: pageSize,
+              }}
+            />
+          </Card>
+        </div>
+        <Pagination
+          itemsCount={customers.length}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      </section>
+      <CustomerDialogDelete />
+      <CustomerDialogEdit />
+      <CustomerDialogAdd />
+    </React.Fragment>
+  );
+};
---
+++ front-end/src/client-react/src/components/customerTable/customerTable.tsx
@@ -0,0 +1,155 @@
+import React from "react";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import Customer from "../../model/customer";
+import {
+  customerDeleteDialogShowed,
+  customerEditDialogShowed,
+  customerSelected,
+} from "../../store/customers";
+import withPixelWidth from "../common/hoc/withPixelWidth";
+import { MenuItem } from "../common/menus/popupMenu/popupMenu";
+import Table, { Paginate } from "../common/tables/table";
+import { Column } from "../common/tables/tableHeader/tableHeader";
+import CustomerTableMenuItems from "../customerTableMenuItems";
+
+interface CustomerTableProps {
+  customers: Customer[];
+  pagination?: Paginate;
+}
+
+const CustomerTable: React.FC<CustomerTableProps> = ({
+  customers,
+  pagination,
+}) => {
+  const dispatch = useAppDispatch();
+
+  const handleDeleteCustomerClick = (customer: Customer) => {
+    dispatch(customerSelected(customer));
+    dispatch(customerDeleteDialogShowed());
+  };
+
+  const handleDetailsCustomerClick = (customer: Customer) => {
+    console.log("Detail clicked");
+  };
+
+  const handleEditCustomerClick = (customer: Customer) => {
+    dispatch(customerSelected(customer));
+    dispatch(customerEditDialogShowed());
+  };
+
+  const menuItems: MenuItem<Customer>[] = [
+    {
+      name: "Details",
+      handler: handleDetailsCustomerClick,
+    },
+    {
+      name: "Delete",
+      handler: handleDeleteCustomerClick,
+    },
+    {
+      name: "Update",
+      handler: handleEditCustomerClick,
+    },
+  ];
+  const columns: Column<Customer>[] = [
+    {
+      name: "Row Key",
+      propName: "rowKey",
+      render: false,
+      sortable: false,
+      responsiveColumn: null,
+    },
+    {
+      name: "Partition Key",
+      propName: "partitionKey",
+      render: false,
+      sortable: false,
+      responsiveColumn: null,
+    },
+    {
+      name: "First Name",
+      propName: "firstName",
+      render: true,
+      sortable: true,
+      responsiveColumn: {
+        hasToRender: false,
+        maxWidth: 130,
+      },
+    },
+    {
+      name: "Last Name",
+      propName: "lastName",
+      render: true,
+      sortable: true,
+      responsiveColumn: {
+        hasToRender: false,
+        maxWidth: 130,
+      },
+    },
+    {
+      name: "Email",
+      propName: "email",
+      render: true,
+      sortable: false,
+      responsiveColumn: {
+        hasToRender: false,
+        maxWidth: 130,
+      },
+    },
+    {
+      name: "Postal Code (EU)",
+      propName: "postalCode",
+      render: true,
+      sortable: true,
+      responsiveColumn: {
+        hasToRender: false,
+        maxWidth: 130,
+      },
+    },
+    {
+      name: "Age",
+      propName: "age",
+      render: true,
+      sortable: true,
+      responsiveColumn: {
+        hasToRender: false,
+        maxWidth: 130,
+      },
+    },
+    {
+      name: "City",
+      propName: "city",
+      render: true,
+      sortable: true,
+      responsiveColumn: {
+        hasToRender: false,
+        maxWidth: 130,
+      },
+    },
+    {
+      name: "",
+      propName: "",
+      render: true,
+      element: (customer: Customer) => (
+        <CustomerTableMenuItems menuItems={menuItems} customer={customer} />
+      ),
+      sortable: false,
+      responsiveColumn: {
+        hasToRender: true,
+        maxWidth: 50,
+      },
+    },
+  ];
+
+  return (
+    <Table
+      items={customers}
+      columnKeys={columns.map((column) => column.propName)}
+      columns={columns}
+      sortColumnPropName={"firstName"}
+      pagination={pagination}
+    />
+  );
+};
+
+export default withPixelWidth(CustomerTable);
